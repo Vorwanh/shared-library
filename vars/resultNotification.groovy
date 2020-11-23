849ee05c094d4089ad829921395c413da6914380
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2020-11-23 18:33:03 +0100

    Update resultNotification.groovy

--- vars/resultNotification.groovy
+++ vars/resultNotification.groovy
@@ -1,8 +1,6 @@
 def call() {
 //Sends Slack notification about the result of the build
-    post {
-        always {
-            notifySlack(currentBuild.result)
-        }
+    always {
+        notifySlack(currentBuild.result)
     }
 }
