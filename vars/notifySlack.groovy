d8e28958a3846ab685bdc28db53de9164988d745
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2020-11-24 10:24:23 +0100

    Update notifySlack.groovy

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -14,13 +14,19 @@ def call(String buildStatus = 'STARTED') {
         color = '#FF9FA1'
     }
     
-    def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+    //def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
     
-    def RESULT_GOOD = "ANALYSIS: `${env.JOB_NAME}` #${env.BUILD_NUMBER}: All good!"
-    def RESULT_BAD = "ANALYSIS: `${env.JOB_NAME}` #${env.BUILD_NUMBER}: Not good at all!"
-    def DEPLOY = "DEPLOY: `${env.JOB_NAME}` #${env.BUILD_NUMBER}: Prepared for modification"
-
     slackSend(color: color, message: msg)
-    
-    
 }
+def call(String message = msg) {
+    
+    def msg
+    
+    if (slackSend == '') {
+        msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}
+    } else if (slackSend == 'RESULT_GOOD') {
+        msg = "ANALYSIS: `${env.JOB_NAME}` #${env.BUILD_NUMBER}: All good!"
+    } else if (slackSend == 'RESULT_GOOD') {
+        msg = "ANALYSIS: `${env.JOB_NAME}` #${env.BUILD_NUMBER}: Not good at all!"
+    } else if (slackSend == 'RESULT_GOOD') {
+        msg = "DEPLOY: `${env.JOB_NAME}` #${env.BUILD_NUMBER}: Prepared for modification"
