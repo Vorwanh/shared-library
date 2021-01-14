1df32a42de005344f86ea812d722d137f1f8b870
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-01-14 16:34:14 +0100

    Update notifySlack.groovy

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,9 +1,9 @@
-
 def call(String buildStatus = 'STARTED') {
     // Build status of null means success.
     buildStatus = buildStatus ?: 'SUCCESS'
 
     def color
+
     if (buildStatus == 'STARTED') {
         color = '#87CEFA'
     } else if (buildStatus == 'SUCCESS') {
@@ -15,19 +15,6 @@ def call(String buildStatus = 'STARTED') {
     }
 
     def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
-  
-    def slack
-    if ("${params.SLACK_NOTIFICATION}" == true) {
-        slackSend(color: color, message: msg)
-        echo "Slack notification sended"
-    }
-    def email
-    if ("${params.EMAIL_NOTIFICATION}" == true) {
-        echo "Email sended"
-    }
-    def sms
-    if ("${params.SMS_NOTIFICATION}" == true) {
-        echo "SMS sended"
-       
-    Send(slack: slack, email: email, sms: sms)
+
+    slackSend(color: color, message: msg)
 }
