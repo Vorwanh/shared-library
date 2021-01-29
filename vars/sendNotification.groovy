5167bb0cc760b51bbf765ce48d615de352889041
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-01-29 19:20:28 +0100

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -20,7 +20,7 @@ def call(String buildStatus = 'STARTED') {
         echo "notify slack works"
     }
     if (params.EMAIL_NOTIFICATION == true) {
-        notifyMail()
+        notifyMail("${params.EMAIL_RECEPIENT_1})
     }
     if (params.SMS_NOTIFICATION == true) {
         notifySMS()
