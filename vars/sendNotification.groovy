52dc48d50c4dc32388a9f66849d1f600838feaf4
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-01-30 14:11:45 +0100

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -16,56 +16,13 @@ def call(String buildStatus = 'STARTED') {
     def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}\n${params.MSG}"
     
     if (params.SLACK_NOTIFICATION == true) {
-        slackSend(color: color, message: msg)
+        //slackSend(color: color, message: msg)
         echo "notify slack works"
     }
     if (params.EMAIL_NOTIFICATION == true) {
-        notifyMail("${params.EMAIL_RECEPIENT_1}")
+        notifyMail(email_recipients: "${params.EMAIL_RECEPIENT_1}", message: msg)
     }
-    if (params.SMS_NOTIFICATION == true) {
-        notifySMS()
-    } 
-}
-def notifyMail(String email_recipients) {
-    echo "send mail works"
-    def mail_list = ["${params.EMAIL_RECEPIENT_1}", "${params.EMAIL_RECEPIENT_2}"]
-    echo "Mail will be send to '${mail_list}'"
-
-    def status, logRegex
-
-    switch (currentBuild.currentResult) {
-        case 'SUCCESS':
-            status = 'successed'
-            logRegex = 'SUCCESS'
-            break
-    
-        case 'UNSTABLE':
-            status = 'unstable'
-            logRegex = 'FAILURE'
-            break
-    
-        case 'FAILURE':
-            status = 'failed'
-            logRegex = 'FAILURE'
-            break
-    //
-        case 'ABORTED':
-            status = 'canceled'
-            logRegex = 'ABORTED'
-            break
-    }
-    emailext(subject: "Build $status - ${JOB_NAME} #${BUILD_NUMBER} ",
-        body: """ <p> Build $status on Job:
-        <a style = "font-size:14px;text-decoration:underline;font-weight:bold" href="${BUILD_URL}/console">${
-            JOB_NAME
-        } - build# ${BUILD_NUMBER} </a></p>
-        <p> <pre> \${BUILD_LOG_REGEX, regex = "^.*?$logRegex.*?\$", linesBefore = 25, linesAfter = 150, maxMatches = 10, showTruncatedLines = false, escapeHtml = false} </pre></p> """
-        , mimeType: 'text/html'
-        , from: '"Jenkins server" <foo@acme.org>'
-        , to: "$email_recipients")
-}
-def notifySMS(String sms_recipients){
-    echo "send SMS works"
-    def sms_list = ["${params.SMS_RECEPIENT_1}", "${SMS_RECEPIENT_2}"]
-    echo "SMS will be send to '${sms_list}'"
+    //if (params.SMS_NOTIFICATION == true) {
+        //notifySMS()
+    //} 
 }
