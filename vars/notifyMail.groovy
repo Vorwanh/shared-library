84e93d3547ce9a7e1d37b25ffb44541500c8128a
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-02-02 13:32:58 +0100

    Update notifyMail.groovy

--- vars/notifyMail.groovy
+++ vars/notifyMail.groovy
@@ -1,4 +1,4 @@
-def call(String email_recipients = "${params.EMAIL_RECEPIENTS_2}", String message = "No message at all") {
+def call(String email_recipients = "jan.pochtiol@gmail.com", String message = "No message at all") {
     wait(5)
     echo "send mail works"
     def mail_list = ["${params.EMAIL_RECEPIENT_1}", "${params.EMAIL_RECEPIENT_2}"]
