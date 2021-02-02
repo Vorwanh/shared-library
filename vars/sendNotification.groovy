aee52fc7f8cd4347c12f7607417b962a90bfaa8a
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-02-02 16:46:56 +0100

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,9 +1,12 @@
-def call(String buildStatus = 'STARTED', email_recipients= '${EMAIL_RECEPIENT_1}', message= "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}\n${params.MSG}") {
+def call(String buildStatus = 'STARTED', email_recipients= '${EMAIL_RECEPIENT_1}', message= "") {
     //Build status of null means success.
     buildStatus = buildStatus ?: 'SUCCESS'
     
     if (email_recipients == "") {
         params.EMAIL_NOTIFICATION = false
+    }
+    if (message == "") {
+        message = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}\n${params.MSG}"
     }
 
     def color
