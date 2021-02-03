c0816e1a756ac5905b8ab23cc223770fd5dbe696
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-02-03 07:48:02 +0100

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,4 +1,4 @@
-def call(String buildStatus = 'STARTED', email_recipients= '${params.EMAIL_RECEPIENT_1}', message= 'Everybody loves Jenkins!') {
+def call(String buildStatus = 'STARTED', email_recipients= ${params.EMAIL_RECEPIENT_1}, message= 'Everybody loves Jenkins!') {
     //Build status of null means success.
     buildStatus = buildStatus ?: 'SUCCESS'
     
