c033e53f2620067fe2bb9a6beee2a4c6941bca26
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-02-03 14:15:10 +0100

    Update notifyMail.groovy

--- vars/notifyMail.groovy
+++ vars/notifyMail.groovy
@@ -1,38 +1,36 @@
 def call(email_recipients = 'jenikns.OASIS@gmail.com',message = 'Probably not everybody.') {
     echo "send mail started"
-    echo "recipients are: ${email_recipients}"
-    echo "message is: ${message}"
 
-    //def status, logRegex
+    def status, logRegex
 
-    //switch (currentBuild.currentResult) {
-        //case 'SUCCESS':
-            //status = 'successed'
-            //logRegex = 'SUCCESS'
-            //break
+    switch (currentBuild.currentResult) {
+        case 'SUCCESS':
+            status = 'successed'
+            logRegex = 'SUCCESS'
+            break
 
-        //case 'UNSTABLE':
-            //status = 'unstable'
-            //logRegex = 'FAILURE'
-            //break
+        case 'UNSTABLE':
+            status = 'unstable'
+            logRegex = 'FAILURE'
+            break
 
-        //case 'FAILURE':
-            //status = 'failed'
-            //logRegex = 'FAILURE'
-            //break
-    //
-        //case 'ABORTED':
-            //status = 'canceled'
-            //logRegex = 'ABORTED'
-            //break
-    //}
-    //emailext(subject: "Build $status - ${JOB_NAME} #${BUILD_NUMBER} ",
-        //body: """ <p> Build $status on Job:
-        //<a style = "font-size:14px;text-decoration:underline;font-weight:bold" href="${BUILD_URL}/console">${
-            //JOB_NAME
-        //} - build# ${BUILD_NUMBER} </a></p>
-        //<p> <pre> \${BUILD_LOG_REGEX, regex = "^.*?$logRegex.*?\$", linesBefore = 25, linesAfter = 150, maxMatches = 10, showTruncatedLines = false, escapeHtml = false} </pre></p> """
-        //, mimeType: 'text/html'
-        //, from: '"Jenkins server" <jenkins.oasis@gmail.com>'
-        //, to: "$email_recipients")
+        case 'FAILURE':
+            status = 'failed'
+            logRegex = 'FAILURE'
+            break
+    
+        case 'ABORTED':
+            status = 'canceled'
+            logRegex = 'ABORTED'
+            break
+    }
+    emailext(subject: "Build $status - ${JOB_NAME} #${BUILD_NUMBER} ",
+        body: """ <p> Build $status on Job:
+        <a style = "font-size:14px;text-decoration:underline;font-weight:bold" href="${BUILD_URL}/console">${
+            JOB_NAME
+        } - build# ${BUILD_NUMBER} </a></p>
+        <p> <pre> \${BUILD_LOG_REGEX, regex = "^.*?$logRegex.*?\$", linesBefore = 25, linesAfter = 150, maxMatches = 10, showTruncatedLines = false, escapeHtml = false} </pre></p> """
+        , mimeType: 'text/html'
+        , from: '"Jenkins server" <jenkins.oasis@gmail.com>'
+        , to: "$email_recipients")
 }
