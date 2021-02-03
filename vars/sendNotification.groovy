a3542716622d427d6c06b79d58e1a04377f78cba
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-02-03 16:32:52 +0100

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,4 +1,4 @@
-def call(String buildStatus = 'STARTED', email_recipients = 'jenkins.OASIS@gmail.com', comment = 'I am changing my mind....') {
+def call(String buildStatus = 'STARTED', String email_recipients = 'jenkins.OASIS@gmail.com', String comment = 'I am changing my mind....') {
     //Build status of null means success.
     buildStatus = buildStatus ?: 'SUCCESS'
 
