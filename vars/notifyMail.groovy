043dcc1838dd883fe6a8ed88ddafa911d2ad824a
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-02-03 16:36:45 +0100

    Update notifyMail.groovy

--- vars/notifyMail.groovy
+++ vars/notifyMail.groovy
@@ -1,8 +1,8 @@
 def call(String email_recipients, String message) {
     echo "send mail started"
 
-    echo email_recipients
-    echo message
+    echo ${email_recipients}
+    echo ${message}
     def status, logRegex
 
     switch (currentBuild.currentResult) {
