dbbcd39e9205b958043b1a4aa5f6f840a9a85528
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-03-01 14:16:41 +0100

    Update notifyMail.groovy

--- vars/notifyMail.groovy
+++ vars/notifyMail.groovy
@@ -27,10 +27,10 @@ def call(String email_recipients = 'jenkins.OASIS@gmail.com', String message = '
             break
     }
     emailext(subject: "Build $status - ${JOB_NAME} #${BUILD_NUMBER} ",
-        body: """ <p> Hey mate! </p> <p> I just wanned to inform you about 2 things. </p> <p> First is that Build ${JOB_NAME} #${BUILD_NUMBER} has $status .</p> <p>You can find more information about the job under the following link: </p>
-        <b><a style = "font-size:14px;text-decoration:underline;font-weight:bold" href="${BUILD_URL}/console">${
+        body: """ <p> Hey mate! </p> <p> I just wanned to inform you about 2 things. </p> <p> First is that the build <b> ${JOB_NAME} #${BUILD_NUMBER} </b> has <b> $status </b>.</p> <p>You can find more information about the job under the following link: </p>
+        <i><a style = "font-size:14px;text-decoration:underline;font-weight:bold" href="${BUILD_URL}/console">${
             JOB_NAME
-        } - build# ${BUILD_NUMBER} </a> </b> </p> <p> Second one is that the shared libraries for notifications works well! Yuhuuuu! </p> <p> And one more thing...! </p> <p> <b> You are awesome! </b> </p>
+        } - build# ${BUILD_NUMBER} </a> </i> </p> <p> Second one is that the shared libraries for notifications works well! Yuhuuuu! </p> <p> And one more thing...! </p> <p> <b> You are awesome! </b> </p>
         <p> <pre> \${BUILD_LOG_REGEX, regex = "^.*?$logRegex.*?\$", linesBefore = 25, linesAfter = 150, maxMatches = 10, showTruncatedLines = false, escapeHtml = false} </pre></p> """
         , mimeType: 'text/html'
         , from: '"Jenkins server" <jenkins.oasis@gmail.com>'
