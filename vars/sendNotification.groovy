5c030a974c427524401a6e04d692daa1273fc811
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-03-01 11:50:53 +0100

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,4 +1,4 @@
-def call(String comment = 'Fear is the path to the dark side.', String email_recipients = 'jenkins.OASIS@gmail.com', String buildStatus = 'STARTED') {
+def call(String comment = '', String email_recipients = 'jenkins.OASIS@gmail.com', String buildStatus = 'STARTED') {
     //Build status of null means success.
     buildStatus = buildStatus ?: 'SUCCESS'
     
@@ -20,7 +20,7 @@ def call(String comment = 'Fear is the path to the dark side.', String email_rec
     def message = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}\n${comment}"
     
     if (params.SLACK_NOTIFICATION == true) {
-        //slackSend(color: color, message: msg)
+        slackSend(color: color, message)
         echo "notify slack works"
     }
     if (params.EMAIL_NOTIFICATION == true) {
