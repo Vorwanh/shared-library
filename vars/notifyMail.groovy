f33a55c935cc084fda5bf812c0751d80b00bfa58
Jan Pochtiol <71016423+Vorwanh@users.noreply.github.com>
2021-03-01 14:05:20 +0100

    Update notifyMail.groovy

--- vars/notifyMail.groovy
+++ vars/notifyMail.groovy
@@ -27,10 +27,10 @@ def call(String email_recipients = 'jenkins.OASIS@gmail.com', String message = '
             break
     }
     emailext(subject: "Build $status - ${JOB_NAME} #${BUILD_NUMBER} ",
-        body: """ <p> Build $status on Job:
+        body: """ <p> Hey mate! I just wanned to tell you that Build ${JOB_NAME} #${BUILD_NUMBER} has $status .You can find more information about the job under the following link:
         <a style = "font-size:14px;text-decoration:underline;font-weight:bold" href="${BUILD_URL}/console">${
             JOB_NAME
-        } - build# ${BUILD_NUMBER} </a></p>
+        } - build# ${BUILD_NUMBER} </a> And do not forget! You are awesome!</p>
         <p> <pre> \${BUILD_LOG_REGEX, regex = "^.*?$logRegex.*?\$", linesBefore = 25, linesAfter = 150, maxMatches = 10, showTruncatedLines = false, escapeHtml = false} </pre></p> """
         , mimeType: 'text/html'
         , from: '"Jenkins server" <jenkins.oasis@gmail.com>'
